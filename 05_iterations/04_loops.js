41b0236fec33186ceafe758c59018a3acb7bd3e8
Biplob Gorai <goraisachin768@gmail.com>
2025-10-19 10:33:44 +0000

    for in loop basics

---
+++ 05_iterations/04_loops.js
@@ -0,0 +1,40 @@
+//for in
+
+const myObject = {
+    js : 'Javascript',
+    cpp : 'C++',
+    rb : 'Ruby',
+    swift : "Swift by Apple"
+}
+
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+// }
+
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+const programming = ["JS", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+
+// const map =  new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
+
+//Map is not iterable
+
